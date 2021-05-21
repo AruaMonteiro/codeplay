8a1c8f810a170b842cdc591f6fa980edcb021afe
Arua Monteiro <aruamonteiro@gmail.com>
2021-05-21 17:43:30 -0300

    Configurando Font Awesome

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 
+gem 'font-awesome-rails', '~> 4.7', '>= 4.7.0.7'
 #A modern CSS framework based on Flexbox
 gem 'bulma-rails', '~> 0.9.1'
 
--- spec/system/visitor_visit_homepage_spec.rb
+++ spec/system/visitor_visit_homepage_spec.rb
@@ -4,8 +4,8 @@ describe 'Visitor visit homepage' do
   it 'successfully' do
     visit root_path
 
-    expect(page).to have_css('h1', text: 'Codeplay')
-    expect(page).to have_css('h3', text: 'Boas vindas ao sistema de gestão de '\
+    expect(page).to have_content('CodePlay')
+    expect(page).to have_content('Boas vindas ao sistema de gestão de '\
                                          'cursos e aulas')
   end
 end
